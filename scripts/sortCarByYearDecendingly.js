e0ba6e118f774a266c0141fe17d1ffcecc833adf
BINTANG RIZQI PASHA <87906435+BintangRP@users.noreply.github.com>
2024-04-01 15:29:20 +0700

    feat: sorting car by year descending

--- scripts/sortCarByYearDecendingly.js
+++ scripts/sortCarByYearDecendingly.js
@@ -1,5 +1,20 @@
+function sortYearDescendingly(cars) {
+  for (let i = 0; i < cars.length - 1; i++) {
+    for (let j = 0; j < cars.length - 1 - i; j++) {
+      if (cars[j].year < cars[j + 1].year) {
+        // sort descendingly
+        // Tukar posisi jika tahun mobil ke-i lebih besar dari tahun mobil ke-(i+1)
+        const temp = cars[j];
+        cars[j] = cars[j + 1];
+        cars[j + 1] = temp;
+      }
+    }
+  }
+}
+
 function sortCarByYearDescendingly(cars) {
   // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log("Sebelum dilakukan sorting descendingly")
   console.log(cars);
 
   // Clone array untuk menghindari side-effect
@@ -7,6 +22,9 @@ function sortCarByYearDescendingly(cars) {
   const result = [...cars];
 
   // Tulis code-mu disini
+  sortYearDescendingly(result)
+  console.log("Setelah dilakukan sorting descendingly")
+  console.log(result)
 
   // Rubah code ini dengan array hasil sorting secara descending
   return result;
