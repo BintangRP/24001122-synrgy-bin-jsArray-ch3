ab4ba6e31d10d36a0740b465db12f75fc9958c3d
BINTANG RIZQI PASHA <87906435+BintangRP@users.noreply.github.com>
2024-04-01 14:56:52 +0700

    initial commit

---
+++ scripts/filterCarByAvailability.js
@@ -0,0 +1,13 @@
+function filterCarByAvailability(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Tempat penampungan hasil
+  const result = [];
+
+  // Tulis code-mu disini
+
+
+  // Rubah code ini dengan array hasil filter berdasarkan availablity
+  return result;
+}
